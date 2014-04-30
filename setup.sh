6e5f02eda8fdd442bbe245471a6a4a35a59b5b53
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-04-30 14:36:10 +0600

    Added chef solo as a install from opscode

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:brightbox/ruby-ng-experimental
-sudo apt-get update
-sudo apt-get install -y ruby2.0
+# Install chef solo
+curl -L https://www.opscode.com/chef/install.sh | bash
+
+#sudo add-apt-repository ppa:brightbox/ruby-ng-experimental
+#sudo apt-get update
+#sudo apt-get install -y ruby2.0
 
 sudo chef-solo -c solo.rb -j node.json
